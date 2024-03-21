89d1a93d8f8b3a2a84391fd109534c17459002e8
pampam <pamelasousaa03@gmail.com>
2024-03-21 18:23:46 -0300

    vivo keyd

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 import bodyParser from "body-parser";
 import express from "express";
 import BuildPdf from "./services/build-pdf.js";
-
-// server config
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-// body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// middleware para restrição de horário
-// app.use(checkHour)
-
+app.get("/", (req, res) => {
+  res.json({home: 'página home'})
+})
 
 const laboratorios = [
   {
@@ -44,12 +40,7 @@ const laboratorios = [
     nome: "Laboratório 6",
     capacidade: 30,
     descricao: "Laboratório de Pesquisa 03",
-  },
-];
-
-app.get("/", (req, res) => {
-  res.json({message: 'Servidor funcionando!'})
-})
+  },];
 
 app.get("/laboratorio/todos", (req, res) => {
   res.json(laboratorios);
@@ -62,14 +53,14 @@ app.post("/laboratorio/novo", (req, res) => {
     if (lab) {
       laboratorios.push(lab);
       res.json({
-        message: `laboratório de nome ${req.body.nome} adicionado!`,
+        message: `laboratório ${req.body.nome} adicionado`,
       });
     } else {
       throw new Error("objeto indefinido ");
     }
   } catch (e) {
     res.json({
-      message: "Ocorreu um erro ao adicionar laboratório",
+      erro: "Ocorreu um erro!",
       error: e.message,
     });
   }
@@ -86,10 +77,10 @@ app.get("/laboratorio/relatorio", (req, res) => {
   BuildPdf(
     (chunk) => stream.write(chunk),
     () => stream.end(),
-    data //passando JSON para o pdf
+    data 
   );
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor Express rodando na porta ${PORT}`);
+  console.log(`localhost:${PORT}`);
 });
